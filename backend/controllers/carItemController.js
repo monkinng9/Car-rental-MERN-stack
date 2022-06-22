0d2a1c26f80796830e32efe8a434d4436a490cc4
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-06-22 16:22:42 +0700

    Express Async Handler

--- backend/controllers/carItemController.js
+++ backend/controllers/carItemController.js
@@ -1,18 +1,25 @@
-const getCarItems = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+const getCarItems = asyncHandler((req, res) => {
   res.status(200).json({ message: 'Get car item' });
-}
+});
 
-const createCarItem = (req, res) => {
+const createCarItem = asyncHandler((req, res) => {
+  if(!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
+
   res.status(200).json({ message: 'Post car item' });
-}
+})
 
-const updateCarItem = (req, res) => {
+const updateCarItem = asyncHandler((req, res) => {
   res.status(200).json({ message: `Update car item ${req.params.id}` });
-}
+})
 
-const deleteCarItem = (req, res) => {
+const deleteCarItem = asyncHandler((req, res) => {
   res.status(200).json({ message: `Delete car item ${req.params.id}` });
-}
+})
 
 module.exports = {
   getCarItems, createCarItem, updateCarItem, deleteCarItem
