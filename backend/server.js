02e4e4e789157827b4c50b8d0ddf0bc63a14f3e3
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-06-22 16:21:20 +0700

    Debug dotenv & Error Middleware Handle

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const dotenv = require('dotenv').config;
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('../backend/middleware/errorMiddleware')
 
 const app = express();
 
 app.use('/api/carItems', require('./routes/carItemRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
